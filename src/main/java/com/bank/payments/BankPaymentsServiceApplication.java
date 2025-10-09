3e91384ed978b9928271d5c3e77fa3e6d952c85d
Said EL ARISS EL IDRISSI <saidariss3@gmail.com>
2025-10-09 08:28:06 +0100

    initial commit

---
+++ src/main/java/com/bank/payments/BankPaymentsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bank.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankPaymentsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankPaymentsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bank/payments/BankPaymentsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankPaymentsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
