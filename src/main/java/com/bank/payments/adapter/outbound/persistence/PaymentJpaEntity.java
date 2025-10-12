504755e141ec8bd2b906cd1abd4a543bd66e7999
Said EL ARISS EL IDRISSI <saidariss3@gmail.com>
2025-10-12 15:27:26 +0100

    fix types between liquibase and jpa

--- src/main/java/com/bank/payments/adapter/outbound/persistence/PaymentJpaEntity.java
+++ src/main/java/com/bank/payments/adapter/outbound/persistence/PaymentJpaEntity.java
@@ -3,6 +3,7 @@ package com.bank.payments.adapter.outbound.persistence;
 import com.bank.payments.domain.model.*;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Entity
 @Setter
 @Getter
+@Table(name = "payments")
 public class PaymentJpaEntity {
     @Id
     private UUID id;
